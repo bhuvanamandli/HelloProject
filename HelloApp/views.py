2d0a589de1b76835ee0d46794a7e3f608bf4b9d8
bhuvanamandli <bhuvana.mandli@gmail.com>
2022-11-05 19:21:26 +0530

    commit

--- HelloApp/views.py
+++ HelloApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def show(request):
-    ss="<h1>Hello this is your dearest person</h1>"
+    ss="<h1>Hello,this is your dearest person</h1>"
     return HttpResponse(ss)
 
 def homepage(request):
